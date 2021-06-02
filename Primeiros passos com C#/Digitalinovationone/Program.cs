d59a026de92055223bfef0359341ec2a8c5e8f7b
Emerson Eduardo Aires Nunes <emersoneduardo.airesnunes@gmail.com>
2021-06-02 15:56:51 -0300

    Criando uma aplicação console

---
+++ Primeiros passos com C#/Digitalinovationone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Digitalinovationone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
